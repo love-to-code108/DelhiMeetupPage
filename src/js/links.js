647a476bc986f01f65daaa7b26f887415263da39
Biswanath Mukherjee <love-to-code108@gmail.com>
2023-07-18 13:57:33 +0530

    the lets connect page is fully functional now

---
+++ src/js/links.js
@@ -0,0 +1,25 @@
+
+// qrcode
+function qrCode(){
+    window.open("https://linktr.ee/EduhubCommunity", "_blank");
+}
+
+// discord
+function discord(){
+    window.open("https://discord.com/invite/RyUs94pSNT", "_blank");
+}
+
+// twitter
+function twitter(){
+    window.open("https://twitter.com/i/flow/login?redirect_after_login=%2FCommunityEduhub", "_blank");
+}
+
+// youtube
+function youtube(){
+    window.open("https://www.youtube.com/channel/UCPpG3qqMH_0QDvSzHyjjZJQ", "_blank");
+}
+
+// linkedin
+function linkedin(){
+    window.open("https://www.linkedin.com/company/eduhub-community", "_blank");
+}
